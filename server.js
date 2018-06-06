111c1d6dc15f1145f44649630ceb3e67d6b50e5a
RoseCrime <31193383+RoseCrime@users.noreply.github.com>
2018-06-06 18:00:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 //Express stuff
 let express = require('express')
-
 let app = express()
 let server = app.listen(80)
-
 app.use(express.static('public'))
 //socket stuff
 let socket = require('socket.io')
-
 let io = socket(server)
-
 //_________________ACTIUAL CODE_______________
 let usersOnline = 0
 
@@ -30,5 +26,5 @@ io.sockets.on('connect', (client) => {
         console.log(client.id + ' disconnected')
         usersOnline--
         console.log(usersOnline)
-    });
-});
+    })
+})
